759865a802ccd0bc2dd9bb9b8efbcbf6ec6277ed
neguscollis12 <neguscollis12@gmail.com>
2026-09-12 15:26:21 -0500

    Adding new test

---
+++ src/test/java/Tests/ListAllBooksTestNegus.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class ListAllBooksTestNegus {
+
+    @Test
+    public void listAllBooksTest() {
+        System.out.println("It works");
+    }
+}
